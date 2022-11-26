34838ca34df86df31fb24bda755858699dfbfed5
Leonardo Duarte <codebrain.leonardo.dasilva@kiwigrid.com>
2022-11-26 16:05:10 -0300

    Exercicio de validar CPF - aula 106

---
+++ ExercicioValidaCPF.js
@@ -0,0 +1,55 @@
+
+class ValidaCPF {
+    constructor(cpfEnviado) {
+        Object.defineProperty(this, 'cpfLimpo', {
+            writable: false,
+            enumerable: true,
+            configurable: false,
+            value: cpfEnviado.replace(/\D+/g, '')
+        })
+    }
+
+    valida(cpfEnviado) {
+        if (!this.cpfLimpo) return false;
+        if (typeof this.cpfLimpo !== 'string') return false;
+        if (this.cpfLimpo.length !== 11) return false;
+        if (this.isSequencia()) return false;
+        this.geraNovoCpf();
+        return this.novoCpf === this.cpfLimpo;
+    }
+
+    isSequencia() {
+        return this.cpfLimpo.charAt(0).repeat(11) === this.cpfLimpo;
+    }
+
+    geraNovoCpf() {
+        const cpfSemDigitos = this.cpfLimpo.slice(0, -2);
+        const digito1 = ValidaCPF.geraDigito(cpfSemDigitos);
+        const digito2 = ValidaCPF.geraDigito(cpfSemDigitos + digito1);
+        this.novoCpf = cpfSemDigitos + digito1 + digito2;
+    }
+
+    static geraDigito(cpfSemDigitos) {
+        let total = 0;
+        let reverso = cpfSemDigitos.length + 1;
+
+        for (let stringNumerica of cpfSemDigitos) {
+            total += reverso * Number(stringNumerica);
+            reverso--;
+        }
+
+        const digito = 11 - (total % 11);
+        return digito <= 9 ? String(digito) : '0';
+    }
+}
+
+const cpf1 = '070.987.720-03';
+// const cpf1 = '999.999.999-99';
+
+const validacpf = new ValidaCPF(cpf1)
+
+if (validacpf.valida()) {
+    console.log('CPF válido');
+} else {
+    console.log('CPF inválido');
+}
